c13fb189c3296645d71c9b7d499c4ab224e08600
ela202 <93539298+ela202@users.noreply.github.com>
2023-02-15 15:21:17 -0500

    drive code

--- src/main/java/frc/robot/subsystems/DriveSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveSubsystem.java
@@ -1,35 +1,83 @@
-
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
-import com.studica.frc.TitanQuad;
+
+import frc.robot.RobotMap;
+
+// import edu.wpi.first.wpilibj.motorcontrol.Talon;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.robot.Constants;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import com.ctre.phoenix.motorcontrol.ControlMode;
 
 /**
- * DriveTrain class 
- * <p>
- * This class creates the instance of the Titan and enables and sets the speed of the defined motor. 
+ * Add your docs here.
  */
-public class DriveTrain extends SubsystemBase
-{
-    private TitanQuad motorR;
-    private TitanQuad motorL;
+public class DriveSubsystem extends SubsystemBase {
 
-    public DriveTrain()
-    {
-        motorR = new TitanQuad(Constants.TITAN_ID, Constants.MOTORR);
-        motorL = new TitanQuad(Constants.TITAN_ID, Constants.MOTORL);
-    }
+  public boolean slow;
+  
+  TalonSRX Left1 = new TalonSRX(RobotMap.Left1);
+  TalonSRX Left2 = new TalonSRX(RobotMap.Left2);
+  TalonSRX Right1 = new TalonSRX(RobotMap.Right1);
+  TalonSRX Right2 = new TalonSRX(RobotMap.Right2);
 
-    /**
-     * Sets the speed of the motor
-     * <p>
-     * @param speed range -1 to 1 (0 stop)
-     */
-    public void setMotorSpeed(double speedR, double speedL)
-    {
-        motorR.set(1);
-        motorL.set(1);
-    }
+  // public void initDefaultCommand() {
+  //   slow = false;
+  // }
+
+  public void TankDrive (double left, double right) {
+
+    // if (slow) {
+    //   left *= 0.80;
+    //   right *= 0.80;
+    // }
+
+    // Drive the left and right sides of the talons
+    Left1.set(ControlMode.PercentOutput,-left);
+    Left2.set(ControlMode.PercentOutput,-left);
+    Right1.set(ControlMode.PercentOutput,right * 0.7);
+    Right2.set(ControlMode.PercentOutput,right * 0.7);
+
+    // Left1.configPeakOutputForward(1);
+    // Left1.configPeakOutputReverse(-1);
+    // Left2.configPeakOutputForward(1);
+    // Left2.configPeakOutputReverse(-1);
+    // Right1.configPeakOutputForward(1);
+    // Right1.configPeakOutputReverse(-1);
+    // Right2.configPeakOutputForward(1);
+    // Right2.configPeakOutputReverse(-1);
+
+
+    
+    // Left1.setInverted(true);
+    // Left2.setInverted(true);
+    // Right1.setInverted(false);
+    // Right2.setInverted(false);
+
+
+    // System.out.println("Running Left:" + left + "\tRunning Right:" + right);
+  }
+
+  // public void setMotorPercent(double percent) {
+
+  //   Left1.set(ControlMode.PercentOutput, percent);
+  //   Left2.set(ControlMode.PercentOutput, percent);
+  //   Right1.set(ControlMode.PercentOutput, percent);
+  //   Right2.set(ControlMode.PercentOutput, percent);
+
+  // }
+
+
+    
+
+
+  public void ArcadeDrive (double speed, double turn) {
+    TankDrive((speed - turn) * 0.5, (speed + turn) * 0.5);
+  }
 
 }
